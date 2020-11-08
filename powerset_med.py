941165ea90a03f36e6b291a34815a51720fad228
Ricky <rickdhanota@gmail.com>
2020-11-08 01:05:57 -0800

    added one solution

--- powerset_med.py
+++ powerset_med.py
@@ -7,5 +7,28 @@
 #Array = [1, 2, 3]
 #[[], [1], [2], [3], [1, 2], [1, 3], [2, 3], [1, 2, 3]]
 
+#O(n*2^n) time | O(n*2^n) space
+# def powerset(array, idx = None):
+#     if idx is None:
+#         idx = len(array) - 1
+#     if idx < 0:
+#         return [[]]
+#     ele = array[idx]
+#     subsets = powerset(array, idx - 1)
+#     for i in range(len(subsets)):
+#         currentSubset = subsets[i]
+#         subsets.append(currentSubset + [ele])
+#     return subsets
+
+
+#O(n*2^n) time | O(n*2^n) space
 def powerset(array):
-    pass
+    subsets = [[]]
+    for ele in array:
+        for i in range(len(subsets)):
+            currentSubset = subsets[i]
+            subsets.append(currentSubset + [ele])
+    return subsets
+
+
+print(powerset([1, 2, 3]))
