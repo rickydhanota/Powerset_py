b423947b531769780deecd2fe00455d465380d25
Ricky <rickdhanota@gmail.com>
2020-11-08 00:49:00 -0800

    initial commit

---
+++ powerset_med.py
@@ -0,0 +1,11 @@
+#Powerset
+
+#Write a function that takes in an array of unique integers and returns its powerset.
+#The powerset P(X) of a set X is the set of all the subsets of X. for example, the powerset of [1, 2] is [[], [1], [2], [1, 2]]
+#Note that the power sets do not need to be in any particular order
+
+#Array = [1, 2, 3]
+#[[], [1], [2], [3], [1, 2], [1, 3], [2, 3], [1, 2, 3]]
+
+def powerset(array):
+    pass
